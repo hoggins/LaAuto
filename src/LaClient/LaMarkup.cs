6d1b659b55e4f4f91f5312dc7f53e39657590e92
Dzen <hoggins.dev@gmail.com>
2020-02-02 19:52:10 +0200

    define mp color range

--- src/LaClient/LaMarkup.cs
+++ src/LaClient/LaMarkup.cs
@@ -15,6 +15,8 @@ namespace LaClient
 
     public static Tuple<Hsv, Hsv> HpRange = Tuple.Create(new Hsv(0, 181, 109), new Hsv(1, 214, 141));
 
+    public static Tuple<Hsv, Hsv> MpRange = Tuple.Create(new Hsv(103, 241, 105), new Hsv(108, 248, 173));
+
     static LaMarkup()
     {
       ShotFrame = Rectangle.Union(Party1Frame, TargetFrame);
