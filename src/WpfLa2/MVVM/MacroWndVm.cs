23ed366d407ad7d54f7fc943ba4b027d29986a99
Dzen <hoggins.dev@gmail.com>
2020-02-02 19:51:52 +0200

    add dumb button to hide buttons

--- src/WpfLa2/MVVM/MacroWndVm.cs
+++ src/WpfLa2/MVVM/MacroWndVm.cs
@@ -34,8 +34,20 @@ namespace WpfLa2.MVVM
     public ObservableCollection<MacroLauncher> MacroLaunchers { get; } = new ObservableCollection<MacroLauncher>();
     public ObservableCollection<MacroWrapper> ActiveMacro { get; } = new ObservableCollection<MacroWrapper>();
 
+    public bool IsCreateVisible
+    {
+      get => _isCreateVisible;
+      set => SetProperty(ref _isCreateVisible, value);
+    }
+
+    public ICommand ToggleCreate { get; }
+
+    private bool _isCreateVisible;
+
     public MacroWndVm()
     {
+      ToggleCreate = new CommandHandler(()=> IsCreateVisible = !IsCreateVisible, () => true);
+
       MacroLaunchers.Add(new MacroLauncher("Test watch window", StartMacro<WatchWndMacro>));
       MacroLaunchers.Add(new MacroLauncher("Heal", StartMacro<HealMacro>));
       MacroLaunchers.Add(new MacroLauncher("Assist", StartMacro<AssistMacro>));
@@ -44,7 +56,7 @@ namespace WpfLa2.MVVM
 
     private void StartMacro<T>() where T : IMacroVm, new()
     {
-      
+
       var macro = new T();
       macro.Start();
       var w = new MacroWrapper(macro, StopMacro);
@@ -55,7 +67,7 @@ namespace WpfLa2.MVVM
     {
       if (ActiveMacro.Remove(wrapper))
         wrapper.Macro?.Stop();
-      else 
+      else
         ; // todo log error
     }
   }
